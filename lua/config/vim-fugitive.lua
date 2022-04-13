a3e198af162cec11f50a1a4dcbfd6b5d97226f11
LHUI <lihui96969@163.com>
2022-04-13 15:40:40 +0800

    refactor: cmap Git command - :Git add . => :ga - :Git commit => :gc

--- lua/config/vim-fugitive.lua
+++ lua/config/vim-fugitive.lua
@@ -1,5 +1,5 @@
 require('utils')
 
 keymap('n', '<leader>g', ':Git<CR>', opts)
-keymap('n', '<leader>ga', ':Git add .<CR>', opts)
-keymap('n', '<leader>gc', ':Git commit<CR>', opts)
+keymap('c', 'ga', 'Git add .<CR>', opts)
+keymap('c', 'gc', 'Git commit<CR>', opts)
